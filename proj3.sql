5ea121821f1b57ab7015315e47d76bd6bc89b852
Leo <lthai04@outlook.com>
2026-06-16 14:48:22 -0400

    table for pokedex, feel free to change it

--- proj3.sql
+++ proj3.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE pokedex (
+    id  INT NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(15) NOT NULL,
+    hp  INT NOT NULL,
+    weakness    VARCHAR(15) NOT NULL,
+    type    VARCHAR(15) NOT NULL,
+    atk1    VARCHAR(15) NOT NULL,
+    atk2    VARCHAR(15) NOT NULL,
+    height  FLOAT NOT NULL,
+    PRIMARY KEY(id)
+);
