8350cdf08df97c671edfc7f15c2ed4671417cd21
Leo <lthai04@outlook.com>
2026-06-22 22:59:25 -0400

    added the sprite and movement around map

--- openworld.js
+++ openworld.js
@@ -11,3 +11,231 @@ background.src = "openStart.png";
         ctx.drawImage(background, 0, 0, bgImg.width, bgImg.height);
     };
 
+
+//binar varaible to check if we started or not
+var started = 0;
+
+//initila spawn when start the game
+//most like will never be used after we implment a save 
+//or will be respawn for the case where ythe trainer lost a battle
+const initSpawnX = 335;
+const initSpawnY = 100;
+
+const spriteImg = new Image();
+spriteImg.src = "sprite.png";
+
+//SPRITES IMG SIZE IS 64
+
+function init(){
+
+    if(started == 0){
+        ctx.drawImage(spriteImg,0 ,0,64, 64,initSpawnX,initSpawnY,64,64);
+        requestAnimationFrame(init);
+    }
+    
+}
+
+init();
+
+
+//made to slow down the sprite
+//updateframe is amount of ticks until spriteframe can be updated
+//baseframe is updated every function call
+const updateFrame = 15;
+var baseFrame = 0;
+
+
+//used to move around the canvas
+var MoveX =335;
+var MoveY =100;
+var dist = .25;
+
+
+//initialized varaible for the start of each frame
+var frameUp = 0;
+var frameDown = 0;
+var frameLeft =0;
+var frameRight =0;
+
+var action = 0;
+
+
+function animateDown(){
+
+    if(MoveY < bgImg.height-45){
+        MoveY += dist;
+    }
+
+    //console.log(MoveY);
+
+
+    ctx.clearRect(0,0,bgImg.width,bgImg.height);
+    ctx.drawImage(background, 0, 0, bgImg.width, bgImg.height);
+    
+    ctx.drawImage(spriteImg,frameDown * 64,0 * 64, 64, 64,MoveX,MoveY,64, 64);
+    
+
+    if(baseFrame % updateFrame == 0){
+    if(frameDown < 3){
+        frameDown++;
+    }else{
+        frameDown = 0;
+    }
+    }
+    baseFrame++;
+
+    if(action == 1){
+    requestAnimationFrame(animateDown);
+    }
+}
+
+
+function animateUp(){
+
+    if(MoveY > 60){
+        MoveY -= dist;
+    }
+
+
+    ctx.clearRect(0,0,bgImg.width,bgImg.height);
+    ctx.drawImage(background, 0, 0, bgImg.width, bgImg.height);
+    
+    ctx.drawImage(spriteImg,frameUp * 64,192, 64, 64,MoveX,MoveY,64, 64);
+    
+
+    if(baseFrame % updateFrame == 0){
+    if(frameUp < 3){
+        frameUp++;
+    }else{
+        frameUp = 0;
+    }
+    }
+    baseFrame++;
+
+    if(action == 1){
+    requestAnimationFrame(animateUp);
+    }
+}
+
+
+
+function animateRight(){
+
+    if(MoveX < 1650){
+        MoveX += dist;
+    }
+
+
+    ctx.clearRect(0,0,bgImg.width,bgImg.height);
+    ctx.drawImage(background, 0, 0, bgImg.width, bgImg.height);
+    
+    ctx.drawImage(spriteImg,frameRight * 64,128, 64, 64,MoveX,MoveY,64, 64);
+    
+
+    if(baseFrame % updateFrame == 0){
+    if(frameRight < 3){
+        frameRight++;
+    }else{
+        frameRight = 0;
+    }
+    }
+    baseFrame++;
+
+    if(action == 1){
+    requestAnimationFrame(animateRight);
+    }
+}
+
+
+function animateLeft(){
+
+    if(MoveX > -15){
+        MoveX -= dist;
+    }
+
+
+    ctx.clearRect(0,0,bgImg.width,bgImg.height);
+    ctx.drawImage(background, 0, 0, bgImg.width, bgImg.height);
+    
+    ctx.drawImage(spriteImg,frameLeft * 64,64, 64, 64,MoveX,MoveY,64, 64);
+    
+
+    if(baseFrame % updateFrame == 0){
+    if(frameLeft < 3){
+        frameLeft++;
+    }else{
+        frameLeft = 0;
+    }
+    }
+    baseFrame++;
+
+    if(action == 1){
+    requestAnimationFrame(animateLeft);
+    }
+}
+
+
+
+
+function stopAnimate() {
+    //clear the sprite
+    ctx.clearRect(0,0,bgImg.width,bgImg.height);
+    ctx.drawImage(background, 0, 0, bgImg.width, bgImg.height);
+    //draw sprite  back with the first sprite animation
+    ctx.drawImage(spriteImg,0 ,0,64, 64,MoveX,MoveY,64,64);
+    //set action back to zero
+    action = 0;
+
+} 
+
+
+document.addEventListener('keydown', function(event){
+
+    started = 1;
+    if(event.key == "w"){
+
+        animateUp();
+        action =1;
+
+    }else if(event.key == "s"){
+
+        animateDown();
+        action =1;
+
+    }else if(event.key == "a"){
+
+        animateLeft();
+        action =1;
+
+    }else if(event.key == "d"){
+
+        animateRight();
+        action =1;
+
+    }
+
+})
+
+//once key up from wasd call stopanimate and in stop animate sets action back to 0
+
+document.addEventListener('keyup', function(event){
+
+    if(event.key == "w"){
+
+        stopAnimate();
+
+    }else if(event.key == "s"){
+
+        stopAnimate();
+
+    }else if(event.key == "a"){
+
+        stopAnimate();
+
+    }else if(event.key == "d"){
+
+        stopAnimate();
+
+    }
+
+})
