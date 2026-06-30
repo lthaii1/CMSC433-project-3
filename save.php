bb38707e96eb1ea5362407863be608fb1d84ebe6
Leo <lthai04@outlook.com>
2026-06-29 22:47:50 -0400

    syntax fixes

--- save.php
+++ save.php
@@ -5,8 +5,10 @@ require_once "database.php";
 $data = json_decode(file_get_contents("php://input"), true);
 
 $sql = "UPDATE players
-        SET current_map = :map, :coord_x = :x, coord_y = :y
-        WHERE name = :name";
+        SET current_map = {$data['map']}, 
+        coord_x = {$data['x']}, 
+        coord_y = {$data['y']}
+        WHERE name = '{$data['playerName']}'";
 
 $pdo->exec($sql);
 
